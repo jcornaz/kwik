538012054af25b173807e0d2ea59aa46d51d997f
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2019-05-16 21:37:04 +0200

    Add SonarQube (and Detekt)
    Fix #10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 @file:Suppress("UNUSED_VARIABLE")
 
+import io.gitlab.arturbosch.detekt.DetektPlugin
+import io.gitlab.arturbosch.detekt.detekt
+import org.sonarqube.gradle.SonarQubeTask
+
 plugins {
     kotlin("multiplatform") version "1.3.31"
     id("org.ajoberstar.reckon") version "0.10.0"
     id("com.github.ben-manes.versions") version "0.21.0"
+    id("io.gitlab.arturbosch.detekt") version "1.0.0-RC14" apply false
+    id("org.sonarqube") version "2.7"
 }
 
 group = "com.github.jcornaz.kwik"
@@ -22,6 +28,7 @@ allprojects {
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.multiplatform")
+    apply<DetektPlugin>()
 
     kotlin {
         jvm()
@@ -54,10 +61,44 @@ subprojects {
             }
         }
     }
+
+    detekt {
+        input = files(
+            "src/commonMain/kotlin",
+            "src/jvmMain/kotlin",
+            "src/commonTest/kotlin",
+            "src/jvmTest/kotlin"
+        )
+        buildUponDefaultConfig = true
+        config = files("$rootDir/detekt-config.yml")
+    }
 }
 
-tasks.register("version") {
-    doLast {
-        println(project.version)
+sonarqube {
+    properties {
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "jcornaz-github")
+        property("sonar.projectKey", "jcornaz_kwik")
+
+        property("sonar.projectVersion", project.version.toString().let {
+            if ('+' in it) it.substringBeforeLast('.') else it
+        })
+
+        property("sonar.kotlin.detekt.reportPaths", "build/reports/detekt/detekt.xml")
+        property("sonar.coverage.exclusions", "**/commonMain/**")
+    }
+}
+
+tasks {
+    register("version") {
+        doLast {
+            println(project.version)
+        }
+    }
+
+    withType<SonarQubeTask> {
+        subprojects.forEach {
+            dependsOn("${it.path}:detekt")
+        }
     }
 }
