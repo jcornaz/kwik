e08281323a1f4c9a68bf07f05f670c86afb32b23
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2019-05-17 20:14:02 +0200

    Publish to bintray only if on master and not in a PR

---
+++ travis_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+    ./gradlew check bintrayUpload sonarqube --no-daemon --no-build-cache
+elif [ "$TRAVIS_PULL_REQUEST" = "false" ] || [ -z "$SONAR_TOKEN" ]
+then
+    ./gradlew check
+else
+    ./gradlew check sonarqube
+fi
