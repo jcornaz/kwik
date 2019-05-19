cdc96ba76604d2f9f1fa15bb0e0fcbeed732f479
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2019-05-19 12:32:48 +0200

    Add usage examples
    Resolve #19

---
+++ core/src/commonTest/kotlin/com/github/jcornaz/kwik/example/PlusOperatorTest.kt
@@ -0,0 +1,44 @@
+package com.github.jcornaz.kwik.example
+
+import com.github.jcornaz.kwik.Generator
+import com.github.jcornaz.kwik.forAll
+import com.github.jcornaz.kwik.generator.ints
+import com.github.jcornaz.kwik.zip
+import kotlin.test.Test
+
+// tag::plus-operator[]
+class PlusOperatorTest {
+
+    // Default config and generators
+    @Test
+    fun isCommutative() = forAll { x: Int, y: Int ->
+        x + y == y + x
+    }
+
+    // With a given number of iterations
+    @Test
+    fun isAssociative() = forAll(iterations = 1000) { x: Int, y: Int, z: Int ->
+        (x + y) + z == x + (y + z)
+    }
+
+    // Use a seed to get reproducible results (useful when investigating a failure in the CI for instance)
+    @Test
+    fun zeroIsNeutral() = forAll(seed = -4567) { x: Int ->
+        x + 0 == x
+    }
+
+    // Configure or use a custom generator
+    @Test
+    fun addNegativeSubtracts() = forAll(Generator.ints(min = 0), Generator.ints(max = 0)) { x, y ->
+        x + y <= x
+    }
+
+    // Create a custom generator
+    val customGenerator1 = Generator.create { rng -> CustomClass(rng.nextInt(), rng.nextInt()) }
+
+    // Combine generators
+    val customGenerator2 = Generator.ints().zip(Generator.ints()) { x, y -> CustomClass(x, y) }
+}
+// tag::plus-operator[]
+
+data class CustomClass(val x: Int, val y: Int)
