f1b095f526efef349fb5ffefcd8ccbe4535e7608
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2019-05-22 22:57:20 +0200

    Don't publish doc from travis

--- travis_script.sh
+++ travis_script.sh
@@ -3,7 +3,7 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-    ./gradlew check bintrayUpload gitPublishPush sonarqube --no-daemon --no-build-cache
+    ./gradlew check bintrayUpload sonarqube --no-daemon --no-build-cache
 elif [ "$TRAVIS_PULL_REQUEST" = "false" ] || [ -z "$SONAR_TOKEN" ]
 then
     ./gradlew check
