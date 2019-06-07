34aabf7d504f2f09cf918ecce5829403005d555f
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2019-06-07 10:31:47 +0200

    Add link to the github repo in doc

--- docs/conf.py
+++ docs/conf.py
@@ -49,3 +49,11 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    "display_github": True,
+    "github_user": "jcornaz",
+    "github_repo": "github",
+    "github_version": "master",
+    "conf_py_path": "/docs/"
+}
