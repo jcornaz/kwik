53cd176e2748cea632bdbf5ae1d4dbb03f125c77
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2019-07-29 18:02:10 +0200

    Make shrinker find smallest failing value

--- evaluator/src/commonMain/kotlin/com/github/jcornaz/kwik/evaluator/Shrinker.kt
+++ evaluator/src/commonMain/kotlin/com/github/jcornaz/kwik/evaluator/Shrinker.kt
@@ -2,4 +2,16 @@ package com.github.jcornaz.kwik.evaluator
 
 import com.github.jcornaz.kwik.generator.api.Generator
 
-internal fun <T> Generator<T>.shrink(initialValue: T, property: (T) -> Boolean): T = initialValue
+internal fun <T> Generator<T>.shrink(initialValue: T, property: (T) -> Boolean): T {
+    var result = initialValue
+
+    var smallerValues = shrink(result)
+    var index = smallerValues.indexOfFirst { !property(it) }
+    while (index >= 0) {
+        result = smallerValues[index]
+        smallerValues = shrink(result)
+        index = smallerValues.indexOfFirst { !property(it) }
+    }
+
+    return result
+}
--- evaluator/src/commonTest/kotlin/com/github/jcornaz/kwik/evaluator/ShrinkerTest.kt
+++ evaluator/src/commonTest/kotlin/com/github/jcornaz/kwik/evaluator/ShrinkerTest.kt
@@ -2,6 +2,7 @@ package com.github.jcornaz.kwik.evaluator
 
 import com.github.jcornaz.kwik.generator.api.Generator
 import com.github.jcornaz.kwik.generator.api.randomSequence
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.fail
@@ -10,17 +11,52 @@ class ShrinkerTest {
 
     @Test
     fun returnsInitialValueIfGeneratorReturnsAnEmptySmallestListOfValue() {
-        val generator = object : Generator<Int> {
-            override val samples: Set<Int> get() = emptySet()
+        val generator = shrinker { emptyList() }
 
-            override fun randoms(seed: Long): Sequence<Int> = randomSequence(seed) { it.nextInt() }
+        repeat(100) {
+            val initialValue = Random.nextInt()
+            assertEquals(
+                expected = initialValue,
+                actual = generator.shrink(initialValue) { fail("property should not be evaluated") }
+            )
+        }
+    }
 
-            override fun shrink(value: Int): List<Int> = emptyList()
+    @Test
+    fun returnsInitialValueIfGeneratorReturnsAllPassingValues() {
+        val generator = shrinker { listOf(1, 2, 3, 4) }
+
+        repeat(100) {
+            val initialValue = Random.nextInt()
+            assertEquals(
+                expected = initialValue,
+                actual = generator.shrink(initialValue) { true }
+            )
+        }
+    }
+
+    @Test
+    fun findsTheSmallestFailingValue() {
+        val generator = shrinker { value ->
+            when (value) {
+                0 -> emptyList()
+                1 -> listOf(0)
+                else -> listOf(value / 2, value - 1)
+            }
         }
 
-        assertEquals(
-            expected = 42,
-            actual = generator.shrink(42) { fail("property should not be evaluated") }
-        )
+        repeat(100) {
+            val initialValue = Random.nextInt(2, 1000)
+            val result = generator.shrink(initialValue) { it < 2 }
+            assertEquals(2, result)
+        }
     }
+}
+
+private fun shrinker(shrinkFct: (Int) -> List<Int>): Generator<Int> = object : Generator<Int> {
+    override val samples: Set<Int> get() = emptySet()
+
+    override fun randoms(seed: Long): Sequence<Int> = randomSequence(seed) { it.nextInt() }
+
+    override fun shrink(value: Int): List<Int> = shrinkFct(value)
 }
