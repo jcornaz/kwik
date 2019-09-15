858b706a14cb312d388de13d1f90f31c98f97377
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2019-09-15 18:22:07 +0200

    Add CheckForAll1

---
+++ evaluator/src/commonTest/kotlin/com/github/jcornaz/kwik/evaluator/CheckForAll1Test.kt
@@ -0,0 +1,82 @@
+package com.github.jcornaz.kwik.evaluator
+
+import com.github.jcornaz.kwik.generator.api.Generator
+import com.github.jcornaz.kwik.generator.api.withSamples
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+class CheckForAll1Test : AbstractRunnerTest() {
+
+    private val testGenerator = Generator.create { it.nextInt() }
+
+    override fun evaluate(iterations: Int, seed: Long, invocation: PropertyEvaluationContext.() -> Boolean) {
+        checkForAll(testGenerator, iterations, seed) { assertTrue(invocation()) }
+    }
+
+    @Test
+    fun evaluateSamples() {
+        val values = mutableSetOf<Int>()
+
+        val gen = Generator.create { it.nextInt(0, 10) }.withSamples(42, 100)
+
+        checkForAll(gen) {
+            values += it
+        }
+
+        assertTrue(42 in values)
+        assertTrue(100 in values)
+    }
+
+    @Test
+    fun falsificationDisplayHelpfulMessage() {
+        val exception = assertFailsWith<FalsifiedPropertyError> {
+            var i = 0
+            checkForAll(
+                Generator.create { 42 },
+                iterations = 123,
+                seed = 78
+            ) { assertTrue(++i < 12) }
+        }
+
+        assertEquals(
+            """
+                Property falsified after 12 tests (out of 123)
+                Argument 1: 42
+                Generation seed: 78
+            """.trimIndent(),
+            exception.message
+        )
+    }
+
+    @Test
+    fun evaluateForRandomValues() {
+        val values = mutableSetOf<Int>()
+
+        checkForAll(testGenerator, seed = 0L) { values += it }
+
+        assertTrue(values.size > 190)
+    }
+
+    @Test
+    fun isPredictable() {
+        val gen = Generator.create { it.nextInt() }
+
+        val pass1 = mutableListOf<Int>()
+        val pass2 = mutableListOf<Int>()
+
+        val seed = 123564L
+
+        checkForAll(gen, seed = seed) { pass1 += it }
+        checkForAll(gen, seed = seed) { pass2 += it }
+
+        assertEquals(pass1, pass2)
+    }
+
+    @Test
+    @Suppress("USELESS_IS_CHECK")
+    fun canBeCalledWithoutExplicitGenerator() {
+        checkForAll { it: Int -> assertTrue(it is Int) }
+    }
+}
