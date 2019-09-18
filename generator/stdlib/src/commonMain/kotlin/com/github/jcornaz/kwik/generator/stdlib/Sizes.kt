c417cbcc3d1fa416d029577375cbde4ffe84a411
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2019-09-18 09:57:33 +0200

    Change default max size to 50

--- generator/stdlib/src/commonMain/kotlin/com/github/jcornaz/kwik/generator/stdlib/Sizes.kt
+++ generator/stdlib/src/commonMain/kotlin/com/github/jcornaz/kwik/generator/stdlib/Sizes.kt
@@ -3,4 +3,4 @@ package com.github.jcornaz.kwik.generator.stdlib
 /**
  * Default max size for string and collection-like generators.
  */
-const val KWIK_DEFAULT_MAX_SIZE = 200
+const val KWIK_DEFAULT_MAX_SIZE = 50
