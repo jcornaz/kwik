e4a6296fa0fc5d26389fbdff88585b5878270a37
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2020-02-08 20:56:28 +0100

    Fix example (when built current version of Kwik)

--- example/settings.gradle.kts
+++ example/settings.gradle.kts
@@ -1,1 +1,18 @@
 rootProject.name = "example"
+
+// This block is only useful to build example against the current version of Kiwk
+// It can be removed in case published version of kwik is used
+if (file("../settings.gradle.kts").exists()) {
+    includeBuild("..") {
+        dependencySubstitution {
+            substitute(module("com.github.jcornaz.kwik:kwik-evaluator-jvm"))
+                .with(project(":evaluator"))
+
+            substitute(module("com.github.jcornaz.kwik:kwik-generator-api-jvm"))
+               .with(project(":generator-api"))
+
+            substitute(module("com.github.jcornaz.kwik:kwik-generator-stdlib-jvm"))
+                .with(project(":generator-stdlib"))
+        }
+    }
+}
