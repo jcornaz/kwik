a12903c45e42f605a9f95765bf636be941c3a9a7
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2020-02-09 13:45:07 +0100

    Add shrinker interface

---
+++ fuzzer/api/src/commonMain/kotlin/com/github/jcornaz/kwik/fuzzer/ExperimentalKwikFuzzer.kt
@@ -0,0 +1,8 @@
+package com.github.jcornaz.kwik.fuzzer
+
+/**
+ * The fuzzer system and related abstractions are extremely experimental and can be completely reworked between
+ * two releases without any announcement or migration aid.
+ */
+@Experimental(Experimental.Level.WARNING)
+annotation class ExperimentalKwikFuzzer
--- fuzzer/api/src/commonMain/kotlin/com/github/jcornaz/kwik/fuzzer/Fuzzer.kt
+++ fuzzer/api/src/commonMain/kotlin/com/github/jcornaz/kwik/fuzzer/Fuzzer.kt
@@ -2,7 +2,19 @@ package com.github.jcornaz.kwik.fuzzer
 
 import com.github.jcornaz.kwik.generator.api.Generator
 
+/**
+ * Definition of how to create values of type [T] that are relevant for testing.
+ *
+ *
+ * This is the combination of
+ * * a [Generator] responsible for value generation,
+ * * a [Shrinker] responsbile for finding smaller/simpler values,
+ * * some [guarantees] that can force the property evaluation to run as long as necessary
+ *   to make sure that all guarantees are fulfilled
+ */
+@ExperimentalKwikFuzzer
 data class Fuzzer<T>(
     val generator: Generator<T>,
+    val shrinker: Shrinker<T>,
     val guarantees: List<(T) -> Boolean>
 )
