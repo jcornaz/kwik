06f0d88972e7918b952717426f1cbeae19c8998d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2020-02-22 22:30:11 +0100

    Test simplificationTree factory

---
+++ generator/api/src/commonTest/kotlin/com/github/jcornaz/kwik/generator/api/simplification/SimplificationTreeTest.kt
@@ -0,0 +1,84 @@
+package com.github.jcornaz.kwik.generator.api.simplification
+
+import com.github.jcornaz.kwik.generator.api.ExperimentalKwikGeneratorApi
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@ExperimentalKwikGeneratorApi
+class SimplificationTreeTest {
+
+    @Test
+    fun rootIsGivenValue() {
+        repeat(100) {
+            val value = Random.nextInt()
+            assertEquals(value, simplificationTree(value) { emptySequence() }.root)
+        }
+    }
+
+    @Test
+    fun buildsBranchesFromSimplifierFunction() {
+        val branches = simplificationTree(42) { sequenceOf(1, 2, 3) }
+            .branches.map { it.root }.toList()
+
+        assertEquals(listOf(1, 2, 3), branches)
+    }
+
+    @Test
+    fun buildsTreeFromSimplifierFunction() {
+        val tree = simplificationTree(4) { value ->
+            when (value) {
+                0 -> emptySequence()
+                1 -> sequenceOf(0)
+                else -> sequenceOf(value / 2, value - 1).distinct()
+            }
+        }
+
+        assertTreeEquals(
+            actual = tree,
+            expected = SimplificationTree(
+                4,
+                sequenceOf(
+                    SimplificationTree(
+                        2, sequenceOf(
+                            SimplificationTree(
+                                1, sequenceOf(
+                                    SimplificationTree(0, emptySequence())
+                                )
+                            )
+                        )
+                    ),
+                    SimplificationTree(
+                        3, sequenceOf(
+                            SimplificationTree(
+                                1, sequenceOf(
+                                    SimplificationTree(0, emptySequence())
+                                )
+                            ),
+                            SimplificationTree(
+                                2, sequenceOf(
+                                    SimplificationTree(
+                                        1, sequenceOf(
+                                            SimplificationTree(0, emptySequence())
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    private fun <T> assertTreeEquals(expected: SimplificationTree<T>, actual: SimplificationTree<T>) {
+        assertEquals(expected.root, actual.root)
+        val actualBranches = actual.branches.toList()
+        val expectedBranches = expected.branches.toList()
+
+        assertEquals(expectedBranches.size, actualBranches.size)
+        expectedBranches.forEachIndexed { index, expectedTree ->
+            assertTreeEquals(expectedTree, actualBranches[index])
+        }
+    }
+}
