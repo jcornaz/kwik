b72fcc51ca0a400ff6c62f143e2d25cc1c2c77bc
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2020-03-28 16:14:58 +0100

    Add missing documentation to data classes properties

--- fuzzer/api/src/commonMain/kotlin/com/github/jcornaz/kwik/fuzzer/api/Fuzzer.kt
+++ fuzzer/api/src/commonMain/kotlin/com/github/jcornaz/kwik/fuzzer/api/Fuzzer.kt
@@ -1,16 +1,16 @@
 package com.github.jcornaz.kwik.fuzzer.api
 
-import com.github.jcornaz.kwik.generator.api.Generator
 import com.github.jcornaz.kwik.fuzzer.api.simplifier.Simplifier
+import com.github.jcornaz.kwik.generator.api.Generator
 
 /**
  * Definition of how to create values of type [T] that are relevant for testing.
  *
  *
  * This is the combination of
- * * a [Generator] responsible for value generation,
- * * a [Simplifier] responsbile for finding smaller/simpler values,
- * * some [guarantees] that can force the property evaluation to run as long as necessary
+ * @property generator The [Generator] responsible for value generation,
+ * @property simplifier The [Simplifier] responsbile for finding smaller/simpler values,
+ * @property guarantees The guarantees that can force the property evaluation to run as long as necessary
  *   to make sure that all guarantees are fulfilled
  */
 @ExperimentalKwikFuzzer
