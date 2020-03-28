f0ee1c2408a0fb7f733ca7dc02b154ce8c57a332
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2020-03-28 20:41:34 +0100

    Fix filter

--- fuzzer/api/src/commonMain/kotlin/com/github/jcornaz/kwik/fuzzer/api/simplifier/SimplifierOperators.kt
+++ fuzzer/api/src/commonMain/kotlin/com/github/jcornaz/kwik/fuzzer/api/simplifier/SimplifierOperators.kt
@@ -8,12 +8,12 @@ import com.github.jcornaz.kwik.fuzzer.api.ExperimentalKwikFuzzer
 @ExperimentalKwikFuzzer
 fun <T> Simplifier<T>.filter(predicate: (T) -> Boolean): Simplifier<T> = object : Simplifier<T> {
     override fun simplify(value: T): Sequence<SimplificationTree<T>> =
-        this@filter.simplify(value).filter { predicate(it.item) }
+        this@filter.simplify(value).mapNotNull { it.filter(predicate) }
 }
 
 /**
  * Returns a simplifier that will never return values matching [predicate] when simplifying.
  */
 @ExperimentalKwikFuzzer
-fun <T> Simplifier<T>.filterNot(predicate: (T) -> Boolean): Simplifier<T> =
+inline fun <T> Simplifier<T>.filterNot(crossinline predicate: (T) -> Boolean): Simplifier<T> =
     filter { !predicate(it) }
--- fuzzer/api/src/commonTest/kotlin/com/github/jcornaz/kwik/fuzzer/api/simplifier/SimplifierFilterTest.kt
+++ fuzzer/api/src/commonTest/kotlin/com/github/jcornaz/kwik/fuzzer/api/simplifier/SimplifierFilterTest.kt
@@ -8,7 +8,7 @@ import kotlin.test.assertEquals
 class SimplifierFilterTest {
 
     @Test
-    fun filterApplyFilterToSequences() {
+    fun keepsOnlyTreesWithRootMatchingThePredicate() {
         val simplifier = simplifier<Int> { value ->
             sequenceOf(1, 2, 3, 4, 5).map { value - it }
         }
@@ -22,9 +22,26 @@ class SimplifierFilterTest {
         assertEquals(listOf(99, 97, 95), list)
     }
 
-
     @Test
-    fun filterApplyFilterNotToSequences() {
+    fun keepsOnlyTreesWithChildrenMatchingThePredicate() {
+        val simplifier = simplifier<Int> { value ->
+            sequenceOf(1, 2, 3).map { value - it }
+        }
+
+        val list = simplifier
+            .filter { it % 2 != 0 }
+            .simplify(100)
+            .flatMap { tree -> tree.children.map { it.item } }
+            .toList()
+
+        assertEquals(listOf(97, 95), list)
+    }
+}
+
+@ExperimentalKwikFuzzer
+class SimplifierFilterNotTest {
+    @Test
+    fun keepsOnlyTreesWithRootNotMatchingThePredicate() {
         val simplifier = simplifier<Int> { value ->
             sequenceOf(1, 2, 3, 4, 5).map { value - it }
         }
@@ -36,5 +53,20 @@ class SimplifierFilterTest {
             .toList()
 
         assertEquals(listOf(99, 97, 95), list)
+    }
+
+    @Test
+    fun keepsOnlyTreesWithChildrenNotMatchingThePredicate() {
+        val simplifier = simplifier<Int> { value ->
+            sequenceOf(1, 2, 3).map { value - it }
+        }
+
+        val list = simplifier
+            .filterNot { it % 2 != 0 }
+            .simplify(100)
+            .flatMap { tree -> tree.children.map { it.item } }
+            .toList()
+
+        assertEquals(listOf(97, 95), list)
     }
 }
