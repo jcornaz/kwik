c49f9e37a8282f87a89d5d870ac29c752cec5825
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2020-04-04 15:33:33 +0200

    Bump sphinx to version 2.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.28.0"
     id("io.gitlab.arturbosch.detekt") version "1.7.2"
     id("com.jfrog.bintray") version "1.8.4" apply false
-    id("kr.motd.sphinx") version "2.6.1"
+    id("kr.motd.sphinx") version "2.9.0"
 }
 
 reckon {
