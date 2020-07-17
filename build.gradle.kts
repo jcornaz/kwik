95c27fdf4bea5f50172bd2c9b5ab2f400eabb244
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2020-07-17 10:28:12 +0200

    chore: fine tune renovate config (#178)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ import java.util.*
 
 plugins {
     `maven-publish`
-    kotlin("multiplatform") version "1.3.72"
+    id("org.jetbrains.kotlin.multiplatform") version "1.3.72"
     id("org.ajoberstar.reckon") version "0.12.0"
     id("com.github.ben-manes.versions") version "0.28.0"
     id("io.gitlab.arturbosch.detekt") version "1.10.0"
