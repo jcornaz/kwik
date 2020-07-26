aed71cc9ab7a3793295b9c230dbc898440c4a5ff
Alvaro Reina <reinabenitez.alvaro@gmail.com>
2020-07-26 22:03:37 +0200

    Test invalid arguments

--- generator/stdlib/src/jvmTest/kotlin/com/github/jcornaz/kwik/generator/stdlib/DateTimeGeneratorTest.kt
+++ generator/stdlib/src/jvmTest/kotlin/com/github/jcornaz/kwik/generator/stdlib/DateTimeGeneratorTest.kt
@@ -6,19 +6,49 @@ import java.time.Duration
 import java.time.Instant
 import java.time.LocalTime
 import java.util.*
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
 
 class DateGeneratorTest : AbstractGeneratorTest() {
     override val generator: Generator<Date> = Generator.dates(Date(0), Date())
+
+    @Test
+    fun failForInvalidMax() {
+        assertFailsWith<IllegalArgumentException> {
+            Generator.dates(Date(), Date(0))
+        }
+    }
 }
 
 class DurationGeneratorTest : AbstractGeneratorTest() {
     override val generator: Generator<Duration> = Generator.durations(Duration.ZERO, Duration.ofDays(100))
+
+    @Test
+    fun failForInvalidMax() {
+        assertFailsWith<IllegalArgumentException> {
+            Generator.durations(Duration.ofSeconds(1), Duration.ZERO)
+        }
+    }
 }
 
 class InstantGeneratorTest : AbstractGeneratorTest() {
     override val generator: Generator<Instant> = Generator.instants(Instant.EPOCH, Instant.now())
+
+    @Test
+    fun failForInvalidMax() {
+        assertFailsWith<IllegalArgumentException> {
+            Generator.instants(Instant.now(), Instant.EPOCH)
+        }
+    }
 }
 
 class LocalTimeGeneratorTest : AbstractGeneratorTest() {
     override val generator: Generator<LocalTime> = Generator.localTimes()
+
+    @Test
+    fun failForInvalidMax() {
+        assertFailsWith<IllegalArgumentException> {
+            Generator.localTimes(LocalTime.MAX, LocalTime.MIN)
+        }
+    }
 }
