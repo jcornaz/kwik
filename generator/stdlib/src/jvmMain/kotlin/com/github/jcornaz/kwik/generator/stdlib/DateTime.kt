e201c6c51515940bf4a26a389bcad52855b3a874
Alvaro Reina <reinabenitez.alvaro@gmail.com>
2020-07-28 21:00:54 +0200

    Update Instant generator

--- generator/stdlib/src/jvmMain/kotlin/com/github/jcornaz/kwik/generator/stdlib/DateTime.kt
+++ generator/stdlib/src/jvmMain/kotlin/com/github/jcornaz/kwik/generator/stdlib/DateTime.kt
@@ -1,6 +1,7 @@
 package com.github.jcornaz.kwik.generator.stdlib
 
 import com.github.jcornaz.kwik.generator.api.Generator
+import com.github.jcornaz.kwik.generator.api.withSamples
 import java.time.Duration
 import java.time.Instant
 import java.time.LocalTime
@@ -8,15 +9,30 @@ import java.time.LocalTime
 private const val DAY_NANOSECONDS = 86400000000000L
 
 /**
- * Returns a generator of [Instant] between [min] (inclusive) and [max] (exclusive) instants
+ * Returns a generator of [Instant] between [min] and [max] (inclusive)
  */
-fun Generator.Companion.instants(min: Instant, max: Instant): Generator<Instant> {
-    require(min.isBefore(max)) {
-        "Max must be after min but min was $min and max was $max"
+fun Generator.Companion.instants(
+    min: Instant = Instant.MIN,
+    max: Instant = Instant.MAX
+): Generator<Instant> {
+    require(max >= min) {
+        "Max must be equal or after min but min was $min and max was $max"
     }
+
+    val range = min..max
+
+    val samples = mutableListOf(min, max)
+
+    if (Instant.EPOCH in range && Instant.EPOCH !in samples) {
+        samples += Instant.EPOCH
+    }
+
     return create { random ->
-        Instant.ofEpochMilli(random.nextLong(from = min.toEpochMilli(), until = max.toEpochMilli()))
-    }
+        Instant.ofEpochSecond(
+            random.nextLong(from = min.epochSecond, until = max.epochSecond),
+            random.nextLong(from = 0, until = 1_000_000_000L)
+        )
+    }.withSamples(samples)
 }
 
 /**
--- generator/stdlib/src/jvmTest/kotlin/com/github/jcornaz/kwik/generator/stdlib/DateTimeGeneratorTest.kt
+++ generator/stdlib/src/jvmTest/kotlin/com/github/jcornaz/kwik/generator/stdlib/DateTimeGeneratorTest.kt
@@ -1,18 +1,20 @@
 package com.github.jcornaz.kwik.generator.stdlib
 
 import com.github.jcornaz.kwik.generator.api.Generator
+import com.github.jcornaz.kwik.generator.api.randomSequence
 import com.github.jcornaz.kwik.generator.test.AbstractGeneratorTest
 import java.time.Duration
 import java.time.Instant
 import java.time.LocalTime
 import kotlin.test.Test
 import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
 
 class DurationGeneratorTest : AbstractGeneratorTest() {
     override val generator: Generator<Duration> = Generator.durations(Duration.ZERO, Duration.ofDays(100))
 
     @Test
-    fun failForInvalidMax() {
+    fun failForInvalidRange() {
         assertFailsWith<IllegalArgumentException> {
             Generator.durations(Duration.ofSeconds(1), Duration.ZERO)
         }
@@ -23,10 +25,68 @@ class InstantGeneratorTest : AbstractGeneratorTest() {
     override val generator: Generator<Instant> = Generator.instants(Instant.EPOCH, Instant.now())
 
     @Test
-    fun failForInvalidMax() {
+    fun failForInvalidRange() {
         assertFailsWith<IllegalArgumentException> {
             Generator.instants(Instant.now(), Instant.EPOCH)
         }
+    }
+
+    @Test
+    fun produceInsideGivenRange() {
+        val min = Instant.now()
+        val max = min.plusSeconds(50)
+        assertTrue(Generator.instants(min = min, max = max).randomSequence(0).take(50).all { it >= min && it <= max })
+    }
+
+    @Test
+    fun doNotProduceEpochIfNotInRange() {
+        val min = Instant.now()
+        val max = min.plusSeconds(50)
+        assertTrue(Generator.instants(min = min, max = max).randomSequence(0).take(50).none { it == Instant.EPOCH })
+    }
+
+    @Test
+    fun doNotProduceFarPastIfNotInRange() {
+        val min = Instant.now()
+        val max = min.plusSeconds(50)
+        assertTrue(Generator.instants(min = min, max = max).randomSequence(0).take(50).none { it == Instant.MIN })
+    }
+
+    @Test
+    fun doNotProduceFarFutureIfNotInRange() {
+        val min = Instant.now()
+        val max = min.plusSeconds(50)
+        assertTrue(Generator.instants(min = min, max = max).randomSequence(0).take(50).none { it == Instant.MAX })
+    }
+
+    @Test
+    fun generateEpoch() {
+        assertTrue(Generator.instants().randomSequence(0).take(50).any { it == Instant.EPOCH })
+    }
+
+    @Test
+    fun generateFarPast() {
+
+        assertTrue(Generator.instants().randomSequence(0).take(50).any { it == Instant.MIN })
+    }
+
+    @Test
+    fun generateFarFuture() {
+        assertTrue(Generator.instants().randomSequence(0).take(50).any { it == Instant.MAX })
+    }
+
+    @Test
+    fun generateMax() {
+        val min = Instant.now()
+        val max = min.plusSeconds(50)
+        assertTrue(Generator.instants(min = min, max = max).randomSequence(0).take(50).any { it == max })
+    }
+
+    @Test
+    fun generateMin() {
+        val min = Instant.now()
+        val max = min.plusSeconds(50)
+        assertTrue(Generator.instants(min = min, max = max).randomSequence(0).take(50).any { it == min })
     }
 }
 
