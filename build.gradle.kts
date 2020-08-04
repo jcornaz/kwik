db8130301ba0b3db781c710861182968c07919bb
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2020-08-04 13:38:49 +0200

    Update version of Kotlin and dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,13 @@ import java.util.*
 
 plugins {
     `maven-publish`
-    id("org.jetbrains.kotlin.multiplatform") version "1.3.72"
+    id("org.jetbrains.kotlin.multiplatform") version "1.4.0-rc"
+    id("org.jetbrains.dokka") version "1.4.0-rc"
     id("org.ajoberstar.reckon") version "0.12.0"
     id("com.github.ben-manes.versions") version "0.29.0"
     id("io.gitlab.arturbosch.detekt") version "1.10.0"
     id("com.jfrog.bintray") version "1.8.5" apply false
     id("kr.motd.sphinx") version "2.9.0"
-    id("org.jetbrains.dokka") version "0.10.1"
 }
 
 reckon {
