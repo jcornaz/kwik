9202301136cc5f8d35b79340cdd6c434150782d8
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2020-08-16 12:04:40 +0200

    Address compiler warnings in withSampleTest

--- generator/api/src/commonTest/kotlin/com/github/jcornaz/kwik/generator/api/WithSampleTest.kt
+++ generator/api/src/commonTest/kotlin/com/github/jcornaz/kwik/generator/api/WithSampleTest.kt
@@ -1,24 +1,26 @@
 package com.github.jcornaz.kwik.generator.api
 
 import com.github.jcornaz.kwik.generator.test.AbstractGeneratorTest
-import kotlin.random.Random
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
+import kotlin.test.assertTrue
 
 class WithSampleTest : AbstractGeneratorTest() {
 
     override val generator: Generator<Int> =
-        Generator { it: Random -> it.nextInt(5, Int.MAX_VALUE) }
+        Generator { it.nextInt(5, Int.MAX_VALUE) }
             .withSamples(1, 2, 3, 4)
 
     @Test
     fun emptyListOfSamplesReturnOriginalGenerator() {
-        val gen = Generator { it: Random -> it.nextInt() }
+        val gen = Generator { it.nextInt() }
         assertSame(gen, gen.withSamples())
     }
 
     @Test
     fun failsIfProbabilityIsBellowZero() {
-        val source = Generator { it: Random -> it.nextInt(5, Int.MAX_VALUE) }
+        val source = Generator { it.nextInt(5, Int.MAX_VALUE) }
         assertFailsWith<IllegalArgumentException> {
             source.withSamples(1, 2, 3, 4, probability = -1.0)
         }
@@ -26,7 +28,7 @@ class WithSampleTest : AbstractGeneratorTest() {
 
     @Test
     fun failsIfProbabilityIsZero() {
-        val source = Generator { it: Random -> it.nextInt(5, Int.MAX_VALUE) }
+        val source = Generator { it.nextInt(5, Int.MAX_VALUE) }
         assertFailsWith<IllegalArgumentException> {
             source.withSamples(1, 2, 3, 4, probability = 0.0)
         }
@@ -34,7 +36,7 @@ class WithSampleTest : AbstractGeneratorTest() {
 
     @Test
     fun failsIfProbabilityOne() {
-        val source = Generator { it: Random -> it.nextInt(5, Int.MAX_VALUE) }
+        val source = Generator { it.nextInt(5, Int.MAX_VALUE) }
         assertFailsWith<IllegalArgumentException> {
             source.withSamples(1, 2, 3, 4, probability = 1.0)
         }
@@ -42,7 +44,7 @@ class WithSampleTest : AbstractGeneratorTest() {
 
     @Test
     fun failsIfProbabilityAboveOne() {
-        val source = Generator { it: Random -> it.nextInt(5, Int.MAX_VALUE) }
+        val source = Generator { it.nextInt(5, Int.MAX_VALUE) }
         assertFailsWith<IllegalArgumentException> {
             source.withSamples(1, 2, 3, 4, probability = 1.01)
         }
@@ -50,7 +52,7 @@ class WithSampleTest : AbstractGeneratorTest() {
 
     @Test
     fun generateSamplesAtGivenProbability() {
-        val sampleCount = Generator { it: Random -> it.nextInt(5, Int.MAX_VALUE) }
+        val sampleCount = Generator { it.nextInt(5, Int.MAX_VALUE) }
             .withSamples(1, 2, 3, 4, probability = 0.5)
             .randomSequence(0)
             .take(1000)
@@ -63,10 +65,10 @@ class WithSampleTest : AbstractGeneratorTest() {
 class WithNullTest : AbstractGeneratorTest() {
 
     override val generator: Generator<Int?> =
-        Generator { it: Random -> it.nextInt(5, Int.MAX_VALUE) }.withNull()
+        Generator { it.nextInt(5, Int.MAX_VALUE) }.withNull()
 
     @Test
     fun generatesNull() {
-        assertTrue(Generator { it: Random -> Any() }.withNull().randomSequence(0).take(50).any { it == null })
+        assertTrue(Generator { Any() }.withNull().randomSequence(0).take(50).any { it == null })
     }
 }
